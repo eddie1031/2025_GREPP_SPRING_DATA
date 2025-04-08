3f29ab79b841452bb82d04be4fdbbb0307f20665
eddie1031 <archaco123@gmail.com>
2025-04-08 12:01:36 +0900

    SELECT, SQL Injection

--- src/test/java/io/eddie/db/query/StatementTests.java
+++ src/test/java/io/eddie/db/query/StatementTests.java
@@ -9,14 +9,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class StatementTests {
 
     Connection conn;
     Statement stmt;
+    ResultSet rs;
 
     @BeforeEach
     void init() {
@@ -25,6 +29,14 @@ public class StatementTests {
 
     @AfterEach
     void close() {
+
+        if ( rs != null ) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.error(e.getMessage());
+            }
+        }
 
         if ( stmt != null ) {
             try {
@@ -68,8 +80,70 @@ public class StatementTests {
     @DisplayName("JDBC, 로그인")
     void selectTest() throws Exception {
 
+        Member user1 = genMember("member", "member");
+        Member user1_ng = genMember("member", "1234");
 
+        // 로그인이 되었다면 -> row가 조회 되어야 함
+        String sql1 = genSelectQuery(user1);
+        String sql2 = genSelectQuery(user1_ng);
 
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery(sql1);
+
+        Member findMember = new Member();
+
+        if ( rs.next() ) {
+            findMember.setMemberId(rs.getInt("member_id"));
+            findMember.setUsername(rs.getString("username"));
+            findMember.setPassword(rs.getString("password"));
+        }
+
+        assertThat(findMember.getMemberId()).isEqualTo(2);
+        assertThat(findMember.getUsername()).isEqualTo("member");
+        assertThat(findMember.getPassword()).isEqualTo("member");
+
+        rs.close();
+
+        rs = stmt.executeQuery(sql2);
+
+        findMember = new Member();
+
+        if ( rs.next() ) {
+            findMember.setMemberId(rs.getInt("member_id"));
+            findMember.setUsername(rs.getString("username"));
+            findMember.setPassword(rs.getString("password"));
+        }
+
+        assertThat(findMember.getUsername()).isNull();
+        assertThat(findMember.getPassword()).isNull();
+
+    }
+
+    @Test
+    @DisplayName("Statement Test, SQL Injection")
+    void statement_test() throws Exception {
+
+        // SELECT m.member_id, m.username, m.password FROM member as m WHERE m.username = 'admin' AND m.password = '' or '' = ''
+        Member admin = genMember("admin", "");
+        String sql = genSelectQuery(admin);
+
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery(sql);
+
+        Member findMember = new Member();
+
+        if ( rs.next() ) {
+            findMember.setMemberId(rs.getInt("member_id"));
+            findMember.setUsername(rs.getString("username"));
+            findMember.setPassword(rs.getString("password"));
+        }
+
+        log.info("findMember = {}", findMember);
+
+    }
+
+    private static String genSelectQuery(Member member) {
+        return "SELECT m.member_id, m.username, m.password FROM member as m WHERE m.username = '%s' AND m.password = '%s'".formatted(member.getUsername(), member.getPassword());
     }
 
     private static String genInsertQuery(Member member) {
